16bc33bbdf67f2108e4bfcc946571eb73cbae797
sarqis <sargis@horizondvp.com>
2021-03-10 23:09:50 +0400

    Initial Commit

---
+++ BrainstormTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BrainstormTask
+//
+//  Created by Sargis Khachatryan on 10.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
